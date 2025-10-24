4c47b695778009afdc55fe917a74707fc2d4ef2d
nadinemarcus <nadinemarcus2002@gmail.com>
2025-10-23 19:58:56 -0700

    Update global.js

--- global.js
+++ global.js
@@ -97,7 +97,7 @@ export async function fetchJSON(url) {
 export function renderProjects(project, containerElement, headingLevel = 'h2') {
   const article = document.createElement('article');
   article.innerHTML = `
-      <h3>${project.title}</h3>
+      <headingLevel>${project.title}</headingLevel>
       
       <img src="${project.image}" alt="${project.title}">
       <p>${project.description}</p>
