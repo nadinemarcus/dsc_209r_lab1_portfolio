a219acc9b8124bee28ed8d63c2a70c1d16cc8168
nadinemarcus <nadinemarcus2002@gmail.com>
2025-10-24 21:16:46 -0700

    lab 4

--- global.js
+++ global.js
@@ -94,13 +94,19 @@ export async function fetchJSON(url) {
   }
 }
 
-export function renderProjects(project, containerElement, headingLevel = 'h2') {
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  projects.forEach(project => {
   const article = document.createElement('article');
   article.innerHTML = `
-      <headingLevel>${project.title}</headingLevel>
+      <${headingLevel}>${project.title}</${headingLevel}>
       
       <img src="${project.image}" alt="${project.title}">
       <p>${project.description}</p>
   `;
   containerElement.appendChild(article);
+});
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
 }
