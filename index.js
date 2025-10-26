94b7ff5b1d736d8996d9462f28febfb8f5a58295
nadinemarcus <nadinemarcus2002@gmail.com>
2025-10-26 16:41:58 -0700

    updating projects

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ const profileStats = document.querySelector('#profile-stats');
 
 if (profileStats) {
   profileStats.innerHTML = `
+    <section class="profile-stats"> 
         <dl>
           <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
           <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
           <dt>Followers:</dt><dd>${githubData.followers}</dd>
           <dt>Following:</dt><dd>${githubData.following}</dd>
         </dl>
-    `;
+    </section>
+        `;
 }
