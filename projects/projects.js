c5049bd827235abda2f835924571e5d2d0ef3840
nadinemarcus <nadinemarcus2002@gmail.com>
2025-10-29 19:52:10 -0700

    lab 5 updates

--- projects/projects.js
+++ projects/projects.js
@@ -10,14 +10,11 @@ const searchInput = document.querySelector('.searchBar');
 let selectedIndex = -1;
 
 function renderPieChart(projectsGiven) {
-  // Clear previous chart & legend
   const svg = d3.select('#projects-pie-plot');
   svg.selectAll('path').remove();
-
   const legend = d3.select('.legend');
   legend.selectAll('li').remove();
 
-  if (projectsGiven.length === 0) return;
 
   const rolledData = d3.rollups(
     projectsGiven,
@@ -26,26 +23,61 @@ function renderPieChart(projectsGiven) {
   ).sort((a, b) => d3.ascending(a[0], b[0]));
 
   const data = rolledData.map(([year, count]) => ({ value: count, label: year }));
-
-  // Arc generator and pie layout
   const arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
   const pieGenerator = d3.pie().value(d => d.value);
   const arcData = pieGenerator(data);
   const colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-   svg.selectAll('path')
+  // slices
+  const paths = svg.selectAll('path')
     .data(arcData)
     .join('path')
     .attr('d', arcGenerator)
-    .attr('fill', (_, i) => colors(i));
+    .attr('fill', (_, i) => colors(i))
+    .style('cursor', 'pointer')
+    .on('click', function(_, d) {
+      const i = arcData.indexOf(d);
+      selectedIndex = selectedIndex === i ? -1 : i;
 
-  // Draw legend
+      // Update classes
+      paths.attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : ''));
+      legend.selectAll('li')
+        .attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : ''));
+
+      // Filter projects
+      if (selectedIndex === -1) {
+        renderProjects(projects, projectsContainer, 'h2');
+      } else {
+        const year = data[selectedIndex].label;
+        const filteredByYear = projects.filter(p => p.year === year);
+        projectsContainer.innerHTML= '';
+        renderProjects(filteredByYear, projectsContainer, 'h2');
+      }
+    });
+
+  // legend
   legend.selectAll('li')
     .data(data)
     .join('li')
-    .html(d => `<span class="swatch" style="background-color:${colors(data.indexOf(d))}"></span> ${d.label} <em>(${d.value})</em>`);
+    .html(d => `<span class="swatch" style="background-color:${colors(data.indexOf(d))}"></span> ${d.label} <em>(${d.value})</em>`)
+    .on('click', (_, d) => {
+      const i = data.indexOf(d);
+      selectedIndex = selectedIndex === i ? -1 : i;
 
+      paths.attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : ''));
+      legend.selectAll('li')
+        .attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : ''));
+
+      if (selectedIndex === -1) {
+        renderProjects(projects, projectsContainer, 'h2');
+      } else {
+        const year = data[selectedIndex].label;
+        const filteredByYear = projects.filter(p => p.year === year);
+        renderProjects(filteredByYear, projectsContainer, 'h2');
+      }
+    });
 }
+
 
 renderProjects(projects, projectsContainer, 'h2');
 renderPieChart(projects);
@@ -62,36 +94,5 @@ searchInput.addEventListener('input', (event) => {
   renderProjects(filteredProjects, projectsContainer, 'h2');
 
   renderPieChart(filteredProjects);
-});
-
-arcData.forEach((d, i) => {
-  svg.append('path')
-    .attr('d', arcGenerator(d))
-    .attr('fill', colors(i))
-    .style('cursor', 'pointer')
-    .on('click', () => {
-      selectedIndex = selectedIndex === i ? -1 : i;
-
-      // Update wedge classes
-      svg
-        .selectAll('path')
-        .attr('class', (_, idx) => (
-            idx === selectedIndex ? 'selected' : ''
-        ));
-
-      // Update legend classes
-      legend
-        .selectAll('li')
-        .attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : ''));
-
-      // Filter projects based on selection
-      if (selectedIndex === -1) {
-        renderProjects(projects, projectsContainer, 'h2');
-      } else {
-        const year = data[selectedIndex].label;
-        const filteredByYear = projects.filter(p => p.year === year);
-        renderProjects(filteredByYear, projectsContainer, 'h2');
-      }
-    });
 });
 
